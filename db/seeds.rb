6377f3784bf947e2cb9ab2145495e05836d96572
Jeffrey Chupp <jeff@semanticart.com>
2012-09-22 12:53:35 -0400

    oops, i-am-a-dummy

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ Player.transaction do
     player['hof'] = true     if player['hof'].try(:strip) == 'hof'
     player['position'] = 'p' if player['position'] == 'rp'
 
-    player['image_url'].delete if player['image_url'].blank?
+    player.delete('image_url') if player['image_url'].blank?
 
     record = Player.find_or_initialize_by_id(player['id'])
     record.update_attributes!(player, as: :admin)
