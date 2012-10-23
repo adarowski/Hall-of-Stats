f4bc98cd6d0075fb8b78dfc33a070f94cf437407
Michael Berkowitz <michael.berkowitz@gmail.com>
2012-10-22 23:02:13 -0400

    Add Similar Players table to players show page.

--- app/models/similarity_score.rb
+++ app/models/similarity_score.rb
@@ -4,6 +4,10 @@ class SimilarityScore < ActiveRecord::Base
   has_and_belongs_to_many :player1s, class_name: 'Player'
   has_and_belongs_to_many :player2s, class_name: 'Player'
 
+  scope :for_player, lambda {|player|
+    where('player1_id = :player_id OR player2_id = :player_id', player_id: player.id).order('score ASC')
+  }
+
   def self.calculator(player1, player2)
     SimilarityCalculator.new(player1, player2).score
   end
@@ -18,5 +22,9 @@ class SimilarityScore < ActiveRecord::Base
     where("(player1_id = :player1_id and player2_id = :player2_id) OR
            (player1_id = :player2_id and player2_id = :player1_id)",
            player1_id: player1.id, player2_id: player2.id).first.try(:score)
+  end
+
+  def players
+    Player.where(:id => [player1_id, player2_id])
   end
 end
