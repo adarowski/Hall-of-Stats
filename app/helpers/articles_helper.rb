89d0c547c13c42d747bde80d334bd1d38fe5f87c
Adam Darowski <adarowski@patientslikeme.com>
2012-11-04 17:55:39 -0500

    Replace dot dot dot with an ellipsis.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,7 +2,7 @@ module ArticlesHelper
   def article_truncation(article)
     BioFormatter.new(
       truncate(article.body, length: 1000, separator: "\n",
-        omission: link_to("Read More...", article_path(article))
+        omission: link_to("Read More", article_path(article), class: 'more')
     )).to_s
   end
 end
