c32eddfbf679b0d26940900340d9704b6b33300a
Adam Darowski <adarowski@patientslikeme.com>
2012-12-07 08:24:17 -0500

    Initial styles and markup tweaks for position pages.

--- app/controllers/position_controller.rb
+++ app/controllers/position_controller.rb
@@ -3,7 +3,9 @@ class PositionController < ApplicationController
     @position = params[:id]
     @position_name = PlayerDecorator::POSITIONS[@position]
     @players = Player.of_position(@position).by_rank.limit(@position == 'p' ? 200 : 100)
-    @best_not_inducted = Player.of_position(@position).not_in_hos.by_rank.limit(5)
+    @best_not_included = Player.of_position(@position).not_in_hos.where(eligibility: 'eligible').by_rank.limit(5)
+    @best_not_inducted = Player.of_position(@position).not_in_hof.where(eligibility: 'eligible').by_rank.limit(5)
     @best_active = Player.of_position(@position).where(eligibility: 'active').by_rank.limit(5)
+    @best_upcoming = Player.of_position(@position).where(eligibility: 'upcoming').by_rank.limit(5)
   end
 end
