2ad863a3ca8f4aad140f2493b4d088686165f951
Adam Darowski <adarowski@patientslikeme.com>
2012-12-07 10:42:03 -0500

    Increase number of players on position lists.

--- app/controllers/position_controller.rb
+++ app/controllers/position_controller.rb
@@ -2,7 +2,7 @@ class PositionController < ApplicationController
   def show
     @position = params[:id]
     @position_name = PlayerDecorator::POSITIONS[@position]
-    @players = Player.of_position(@position).by_rank.limit(@position == 'p' ? 200 : 100)
+    @players = Player.of_position(@position).by_rank.limit(@position == 'p' ? 500 : 200)
     @best_not_included = Player.of_position(@position).not_in_hos.where(eligibility: 'eligible').by_rank.limit(5)
     @best_not_inducted = Player.of_position(@position).not_in_hof.where(eligibility: 'eligible').by_rank.limit(5)
     @best_active = Player.of_position(@position).where(eligibility: 'active').by_rank.limit(5)
