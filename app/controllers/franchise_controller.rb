818ae67ccf65ffeb2e2bac9afadd9c8b590feb01
Michael Berkowitz <michael.berkowitz@gmail.com>
2013-03-13 13:28:58 -0400

    Add first draft of charts page to franchises

--- app/controllers/franchise_controller.rb
+++ app/controllers/franchise_controller.rb
@@ -21,4 +21,31 @@ class FranchiseController < ApplicationController
     end
     render @players
   end
+
+  def charts
+  end
+
+  def all_data
+    sql = <<-SQL
+select stats.franchise_id, range_year, coalesce(sum(ss.hall_rating), 0) as sum 
+from generate_series(
+  (select min(year) from season_stats),
+  (select max(year) from season_stats),
+  1) as range_year 
+cross join (
+  select distinct(franchise_id) from season_stats
+) as stats 
+left join season_stats ss 
+on stats.franchise_id = ss.franchise_id and range_year = ss.year 
+group by range_year, stats.franchise_id 
+order by stats.franchise_id, range_year
+    SQL
+    data = SeasonStats.find_by_sql(sql)
+    sorted_data = Franchise.active.map do |franchise|
+      { key: franchise.name,
+        values: data.select{|d| d.franchise_id == franchise.id}.map{|d| [d.range_year.to_i, d.sum.to_f]}
+      } unless data.select{|d| d.franchise_id == franchise.id}.size == 0
+    end.compact
+    render json: sorted_data
+  end
 end
