f42c7d0cc179464f0ee7a973b34726c1e878bc1b
Michael Berkowitz <michael.berkowitz@gmail.com>
2013-03-14 20:33:06 -0400

    Add some progress stats to seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 require 'csv'
 
 Player.transaction do
-  CSV.parse(File.open("public/hall-of-stats-new.csv"), headers: true).each do |row|
+  puts "Creating Players..."
+  CSV.parse(File.open("public/hall-of-stats-new.csv"), headers: true).each_with_index do |row, idx|
+    puts idx if idx % 100 == 0
     player = row.to_hash
 
     player['id'] = player.delete('player_id')
@@ -17,18 +19,23 @@ Player.transaction do
     record = Player.find_or_initialize_by_id(player['id'])
     record.update_attributes!(player, as: :admin)
   end
+  puts "Done creating players"
 end
 
 SeasonStats.transaction do
+  puts "Creating SeasonStats..."
   CSV.parse(File.open('public/hall-of-stats-franchise-seasons.csv'), headers: true).each_with_index do |row, idx|
+    puts idx if idx % 100 == 0
     stats = row.to_hash
     stats.merge!(franchise_id: stats['franchise_id'].downcase)
     record = SeasonStats.new
     record.update_attributes!(stats, as: :admin)
   end
+  puts "Done creating season stats"
 end
 
 FranchiseRating.transaction do
+  puts "Creating FranchiseRatings..."
   CSV.parse(File.open('public/franchise-hall-rating.csv'), headers: true).each_with_index do |row, idx|
     puts idx if idx % 100 == 0
     stats = row.to_hash
@@ -37,6 +44,7 @@ FranchiseRating.transaction do
                                    franchise_id: stats['franchise_id']).first_or_initialize
     record.update_attributes!(stats, as: :admin)
   end
+  puts "Done creating franchise ratings"
 end
 
 if AdminUser.where(email: 'admin@example.com').blank?
