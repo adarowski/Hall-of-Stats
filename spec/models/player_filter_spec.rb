8338ec2bd64374526161ad1d57d7d9a5f41fcfbd
Michael Berkowitz <michael.berkowitz@gmail.com>
2013-07-02 16:43:01 -0400

    Update tests to account for Player#personal_hof

--- spec/models/player_filter_spec.rb
+++ spec/models/player_filter_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 describe PlayerFilter do
-  let(:player_one) { create :player, position: 'p', hos: true, hof: false, hall_rating: 120, eligibility: 'active' }
+  let(:player_one) { create :player, position: 'p', hos: true, personal_hof: true, hof: false, hall_rating: 120, eligibility: 'active' }
   let(:player_two) { create :player, position: '1b', hos: false, hof: false, hall_rating: 95, eligibility: 'active' }
   let(:player_three) { create :player, position: 'c', hos: false, hof: false, hall_rating: 110, eligibility: 'upcoming' }
   let(:player_four) { create :player, position: 'p', hos: false, hof: false, hall_rating: 120, eligibility: 'active' }
 
   subject(:filter) { PlayerFilter }
 
-  it { filter.filters_for(player_one).should == %w( p not-hof hos ) }
-  it { filter.filters_for(player_two).should == %w( 1b not-hof not-hos ) }
-  it { filter.filters_for(player_three).should == %w( c not-hof not-hos upcoming ) }
-  it { filter.filters_for(player_four).should == %w( p not-hof not-hos active-and-worthy ) }
+  it { filter.filters_for(player_one).should == %w( p not-hof hos personal-hof ) }
+  it { filter.filters_for(player_two).should == %w( 1b not-hof not-hos not-personal-hof active-and-close ) }
+  it { filter.filters_for(player_three).should == %w( c not-hof not-hos not-personal-hof upcoming ) }
+  it { filter.filters_for(player_four).should == %w( p not-hof not-hos not-personal-hof active-and-worthy ) }
 end
