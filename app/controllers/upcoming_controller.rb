1ea686ab1cc7252b145b27143cbc9df68b6cf106
Adam Darowski <adarowski@gmail.com>
2018-10-31 20:06:50 -0400

    2018

--- app/controllers/upcoming_controller.rb
+++ app/controllers/upcoming_controller.rb
@@ -7,6 +7,7 @@ class UpcomingController < ApplicationController
     @eligible_2021 = Player.eligible_2021.by_rank
     @eligible_2022 = Player.eligible_2022.by_rank
     @eligible_2023 = Player.eligible_2023.by_rank
+    @eligible_2024 = Player.eligible_2024.by_rank
     @tg_era_2019 = Player.tg_era_2019.by_rank
     @mb_era_2020 = Player.mb_era_2020.by_rank
     @gd_era_2021 = Player.gd_era_2021.by_rank
