25baa984b40acfa0d8cc3a01cf1f7dd03ce68080
Michael Berkowitz <michael.berkowitz@gmail.com>
2020-12-18 16:28:17 -0500

    ignore touchpunch for now?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require jquery.ui.mouse
 //= require jquery-ui-autocomplete-html
-//= require jquery.ui.touch-punch
 //= require d3
 //= require filters
 //= require players
